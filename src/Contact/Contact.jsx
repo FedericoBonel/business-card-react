4778419e92ed8a85867c935d57646b803f32b8f9
Federico Jorge Bonel Tozzi <Bonelfederico@gmail.com>
2022-07-27 12:04:40 +0900

    Fixed contact buttons width

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -5,23 +5,11 @@ import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 const Contact = () => {
     return (
         <div className="contact-container">
-            <a
-                href="mailto:bonelfederico@gmail.com"
-                target="_blank"
-                rel="noreferrer"
-                id="email-btn"
-                className="contant-container__btn"
-            >
+            <a href="bonelfederico@gmail.com" target="_blank" id="email-btn" className="contant-container__btn">
                 <FontAwesomeIcon icon={faEnvelope} id="email-icon" />
                 Email
             </a>
-            <a
-                href="https://www.linkedin.com/in/federico-jorge-bonel-tozzi/"
-                target="_blank"
-                rel="noreferrer"
-                id="linkedIn-btn"
-                className="contant-container__btn"
-            >
+            <a href="https://linkedin.com/" target="_blank" id="linkedIn-btn" className="contant-container__btn">
                 <FontAwesomeIcon icon={faLinkedin} id="linkedIn-icon" />
                 LinkedIn
             </a>
